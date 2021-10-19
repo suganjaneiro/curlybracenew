b361e6fa2de1e6368b63311572a85b2f8e95231a
suganjaneiro <suganthimary1682000@gmail.com>
2021-10-19 13:43:37 +0530

    subscribe_page

---
+++ curlybrace/public/assets/js/subscribe.js
@@ -0,0 +1,28 @@
+
+let submitted = false;
+
+function validateEmail() {
+    let email = document.getElementById("email").value;
+    if (email.length == 0) {
+        alert("Email can't be blank"); //Validation Message
+        return false;
+    }
+
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        alert("Please enter a correct email address"); //Validation Message
+        return false;
+    }
+
+    return true;
+}
+
+function validateForm() {
+    if (!validateEmail()) {
+        alert("Form not submitted"); //Validation Message
+        return false;
+    } else {
+        alert("Thank you! You have subscribed our page");
+        submitted = true;
+        return true;
+    }
+}
